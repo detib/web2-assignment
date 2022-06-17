8b2a255a4fe4db813a3f53b87657bdf32db298f5
detib <detibaholli@gmail.com>
2022-06-17 17:19:43 +0200

    configured remote database

--- config/database.php
+++ config/database.php
@@ -1,8 +1,8 @@
 <?php
     define("DB_HOST", "remotemysql.com");
-    define("DB_USER", "xx9tjDv0Fg");
-    define("DB_PASS", "MuzHmV4ZdO");
-    define("DB_NAME", "xx9tjDv0Fg");
+    define("DB_USER", "cjYy74pr42");
+    define("DB_PASS", "wevo1PivUY");
+    define("DB_NAME", "cjYy74pr42");
     
     $conn = mysqli_connect(DB_HOST, DB_USER, DB_PASS, DB_NAME);
 
